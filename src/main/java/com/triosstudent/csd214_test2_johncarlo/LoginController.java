66cda1a14042a689a33a23c89517f7702894d872
JohnCarloHipolito <Johncarlo.Hipolito@triosstudent.com>
2024-02-23 14:39:44 -0500

    Section C: Controller and View Implementation

--- src/main/java/com/triosstudent/csd214_test2_johncarlo/LoginController.java
+++ src/main/java/com/triosstudent/csd214_test2_johncarlo/LoginController.java
@@ -1,7 +1,62 @@
 package com.triosstudent.csd214_test2_johncarlo;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class LoginController {
+
+    public TextField emailTF;
+    public PasswordField passwordTF;
+    public Label messageLbl;
+
+    @FXML
+    protected void onLogin() {
+        // validate email and password
+        if(emailTF.getText().isEmpty() || passwordTF.getText().isEmpty()) {
+            messageLbl.setText("Please provide email and password");
+            return;
+        }
+        // connect to database
+        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/csd214_test2_johncarlo", "admin", "admin")) {
+            String readQuery = "SELECT * FROM account_info WHERE email='%s' AND password='%s'";
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(String.format(readQuery, emailTF.getText(), passwordTF.getText()));
+            if (resultSet.next()) {
+                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("order-view.fxml"));
+                Parent root = fxmlLoader.load();
+                OrderController inventoryController = fxmlLoader.getController();
+                inventoryController.setData(new AccountInfo(
+                        resultSet.getLong("id"),
+                        resultSet.getString("username"),
+                        resultSet.getString("email"),
+                        resultSet.getString("password")));
+                Stage inventoryStage = new Stage();
+                inventoryStage.setScene(new Scene(root));
+                inventoryStage.setTitle("CSD214_Test2 John Carlo - Order Management System");
+                inventoryStage.show();
+
+                Stage loginStage = (Stage) emailTF.getScene().getWindow();
+                loginStage.close();
+            } else {
+                messageLbl.setText("Login failed.");
+            }
+        } catch (SQLException e) {
+            System.out.println("SQL Error: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("FXML Error: " + e.getMessage());
+        }
+    }
 }
--- src/main/java/com/triosstudent/csd214_test2_johncarlo/OrderController.java
+++ src/main/java/com/triosstudent/csd214_test2_johncarlo/OrderController.java
@@ -1,4 +1,50 @@
 package com.triosstudent.csd214_test2_johncarlo;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Hyperlink;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class OrderController {
+
+    @FXML private Hyperlink logoutHL;
+    private AccountInfo accountInfo;
+
+    // receive the data from login stage
+    public void setData(AccountInfo accountInfo) {
+        this.accountInfo = accountInfo;
+    }
+
+    @FXML
+    protected void executeQuery(ActionEvent event) {
+    }
+
+    @FXML
+    protected void onLogout() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(OrderMgmtApplication.class.getResource("login-view.fxml"));
+        Stage orderStage = (Stage) logoutHL.getScene().getWindow();
+        Stage stage = new Stage();
+        stage.setTitle("CSD214 Test2: John Carlo - Inventory Management System");
+        stage.setScene(new Scene(fxmlLoader.load()));
+        orderStage.close();
+        stage.show();
+    }
+
+    private void readItems(Connection connection) throws SQLException {
+    }
+
+    private void createItem(Connection connection) throws SQLException {
+    }
+
+    private void updateItem(Connection connection) throws SQLException {
+    }
+
+    private void deleteItem(Connection connection) throws SQLException {
+    }
 }
