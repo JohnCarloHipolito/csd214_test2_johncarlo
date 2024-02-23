1e5dbf1ca88f2600322667765e5a3fdf513be2e2
JohnCarloHipolito <Johncarlo.Hipolito@triosstudent.com>
2024-02-23 14:21:34 -0500

    Section E: Data Modelling

---
+++ src/main/java/com/triosstudent/csd214_test2_johncarlo/OderDetails.java
@@ -0,0 +1,48 @@
+package com.triosstudent.csd214_test2_johncarlo;
+
+public class OderDetails {
+
+    private Long orderId;
+    private String productName;
+    private Integer quantity;
+    private Double totalPrice;
+
+    public OderDetails(Long orderId, String productName, Integer quantity, Double totalPrice) {
+        this.orderId = orderId;
+        this.productName = productName;
+        this.quantity = quantity;
+        this.totalPrice = totalPrice;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(Double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+}
